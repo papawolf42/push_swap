9ff71e817a69dcebd0be5fcc630a32fa09924745
papawolf42 <papawolf@kakao.com>
2021-06-28 01:13:54 +0900

    21.06.28 Fix the problem that the dividing line was printed incorrectly

--- src/simulator.c
+++ src/simulator.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <gunkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 00:03:26 by gunkim            #+#    #+#             */
-/*   Updated: 2021/06/27 21:35:48 by gunkim           ###   ########.fr       */
+/*   Updated: 2021/06/28 01:11:29 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ int	main(int argc, char *argv[])
 	{
 		if (ft_check_end(&ctrl))
 			break ;
-		ft_print_devider_line();
 		ft_process_command(&ctrl);
 		ft_print_stack(&ctrl);
 	}
