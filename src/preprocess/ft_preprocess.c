2e5f2b268c193a41952cd8d294a8b027f65628de
papawolf42 <papawolf@kakao.com>
2021-06-28 01:24:49 +0900

    21.06.28 Fix double free at simulator

--- src/preprocess/ft_preprocess.c
+++ src/preprocess/ft_preprocess.c
@@ -6,7 +6,7 @@
 /*   By: gunkim <gunkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 18:27:58 by gunkim            #+#    #+#             */
-/*   Updated: 2021/06/26 10:27:13 by gunkim           ###   ########.fr       */
+/*   Updated: 2021/06/28 01:17:18 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static t_bool	ft_free_static(size_t depth, char ***splits
 	else if (depth == 2)
 		return (ft_destroy_splits(splits)
 			&& ft_destroy_array(array) && ft_destroy_array(array_sorted));
-	return (0);
+	return (fail);
 }
 
 t_bool	ft_preprocess(t_ctrl *ctrl, int argc, char **argv)
@@ -37,7 +37,7 @@ t_bool	ft_preprocess(t_ctrl *ctrl, int argc, char **argv)
 	if (ft_parse_string(ctrl, splits, &array, &array_sorted) == fail)
 		return (ft_free_static(1, splits, array, array_sorted));
 	if (ft_validate_duplicate(ctrl, array_sorted) == fail)
-		return (ft_free_static(2, splits, array, array_sorted));
+		return (ft_free_static(1, splits, array, array_sorted));
 	if (ft_init_stack(ctrl, array, array_sorted))
 		return (ft_free_static(2, splits, array, array_sorted));
 	ft_free_static(2, splits, array, array_sorted);
