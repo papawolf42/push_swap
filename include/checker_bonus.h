d50264dfddfa72c9dfd8e161a292f87acaf84466
papawolf42 <papawolf@kakao.com>
2021-06-28 14:22:53 +0900

    21.06.28 Norm check the forgotten header files

--- include/checker_bonus.h
+++ include/checker_bonus.h
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   checker_bonus.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gunkim <gunkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 23:19:15 by gunkim            #+#    #+#             */
-/*   Updated: 2021/06/27 15:37:26 by gunkim           ###   ########.fr       */
+/*   Updated: 2021/06/28 14:20:37 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECKER_BONUS_H
 # define CHECKER_BONUS_H
 
-# define	GNL_SUCCESS		1
-# define	GNL_END			0
-# define	GNL_FAIL		-1
+# define GNL_SUCCESS		1
+# define GNL_END			0
+# define GNL_FAIL			-1
 
 # include "structure.h"
 
